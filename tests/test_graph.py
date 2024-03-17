807d88b5a428db28a84e8a1b2bfd75a939208f1e
Zakhar Kalachev <itiszakk@gmail.com>
2024-03-17 03:58:21 +0300

    Graph and utils tests

--- tests/test_graph.py
+++ tests/test_graph.py
@@ -1,23 +1,7 @@
 import pytest
 
-from pathfinding.core.cell import Cell, CellState
 from pathfinding.core.direction import Direction
-from pathfinding.core.graph import Graph
-from pathfinding.core.vector import Vector2D
-from pathfinding.world.world_element import WorldElement
-
-
-class TestElement(WorldElement):
-    def __init__(self, entity, cell):
-        super().__init__(entity)
-        self.cell = cell
-
-    def get_cell(self) -> Cell | None:
-        return self.cell
-
-
-def cell(state: CellState):
-    return Cell(Vector2D(0, 0), 10, 10, state)
+from pathfinding.core.graph import Graph, Vertex
 
 
 @pytest.fixture
@@ -27,52 +11,36 @@ def graph():
 
 @pytest.fixture
 def origin():
-    return TestElement(1, cell(CellState.SAFE))
+    return Vertex(1)
 
 
-def test_create_edge_with_present_safe_destination(graph, origin):
-    destination = TestElement(2, cell(CellState.SAFE))
-    graph.create_edge(origin, Direction.N, [destination])
-    assert graph.graph[origin][Direction.N] == [destination]
+@pytest.fixture
+def destinations():
+    return [Vertex(2), Vertex(3)]
 
 
-def test_create_edge_with_present_mixed_destination(graph, origin):
-    destination = TestElement(2, cell(CellState.MIXED))
-    graph.create_edge(origin, Direction.N, [destination])
-    assert graph.graph[origin][Direction.N] == []
+def test_add_edge(graph, origin, destinations):
+    graph.add_edge(origin, Direction.N, [destinations[0]])
+    assert graph.graph[origin][Direction.N] == [destinations[0]]
 
 
-def test_create_edge_with_present_unsafe_destination(graph, origin):
-    destination = TestElement(2, cell(CellState.UNSAFE))
-    graph.create_edge(origin, Direction.N, [destination])
-    assert graph.graph[origin][Direction.N] == []
+def test_neighbour(graph, origin, destinations):
+    graph.add_edge(origin, Direction.N, [destinations[0]])
+    assert graph.neighbour(origin, Direction.N) == destinations[0]
 
 
-def test_create_edge_with_missing_destination(graph, origin):
-    graph.create_edge(origin, Direction.N, [])
-    assert not graph.graph[origin][Direction.N]
-
-
-def test_neighbour_if_missing(graph, origin):
+def test_neighbour_nonexistent(graph, origin):
     assert graph.neighbour(origin, Direction.N) is None
 
 
-def test_neighbour_if_exists(graph, origin):
-    destination = TestElement(2, cell(CellState.SAFE))
-    graph.create_edge(origin, Direction.N, [destination])
-    assert graph.neighbour(origin, Direction.N) == destination
-
-
-def test_neighbours_if_missing(graph, origin):
-    assert graph.neighbours(origin) == []
-
-
-def test_neighbours_if_exists(graph, origin):
-    destination_1 = TestElement(2, cell(CellState.SAFE))
-    destination_2 = TestElement(3, cell(CellState.SAFE))
-    graph.create_edge(origin, Direction.N, [destination_1])
-    graph.create_edge(origin, Direction.E, [destination_2])
+def test_neighbours(graph, origin, destinations):
+    graph.add_edge(origin, Direction.N, [destinations[0]])
+    graph.add_edge(origin, Direction.E, [destinations[1]])
     neighbours = graph.neighbours(origin)
     assert len(neighbours) == 2
-    assert destination_1 in neighbours
-    assert destination_2 in neighbours
+    assert destinations[0] in neighbours
+    assert destinations[1] in neighbours
+
+
+def test_neighbours_nonexistent(graph, origin):
+    assert graph.neighbours(origin) == []
---
+++ tests/test_utils.py
@@ -0,0 +1,56 @@
+import pytest
+
+from pathfinding.core.cell import Cell, CellState
+from pathfinding.core.vector import Vector2D
+from pathfinding.exception import PathPointIsUnsafeException
+from pathfinding.utils import check_points
+from pathfinding.world.world_element import WorldElement
+
+
+class TestWorldElement(WorldElement):
+    def __init__(self, entity, state):
+        super().__init__(entity)
+        self.cell = Cell(Vector2D(0, 0), 10, 10, state)
+
+    def get_cell(self) -> Cell | None:
+        return self.cell
+
+
+@pytest.fixture
+def start_point():
+    return Vector2D(0, 0)
+
+
+@pytest.fixture
+def end_point():
+    return Vector2D(100, 100)
+
+
+@pytest.fixture
+def safe_element():
+    return TestWorldElement(0, CellState.SAFE)
+
+
+@pytest.fixture
+def unsafe_element():
+    return TestWorldElement(1, CellState.UNSAFE)
+
+
+@pytest.fixture
+def mixed_element():
+    return TestWorldElement(2, CellState.MIXED)
+
+
+def test_check_point_safe(start_point, end_point, safe_element):
+    check_points(start_point, end_point, safe_element, safe_element)
+    # No exception should be raised
+
+
+def test_check_point_unsafe(start_point, end_point, unsafe_element):
+    with pytest.raises(PathPointIsUnsafeException):
+        check_points(start_point, end_point, unsafe_element, unsafe_element)
+
+
+def test_check_point_mixed(start_point, end_point, mixed_element):
+    with pytest.raises(PathPointIsUnsafeException):
+        check_points(start_point, end_point, mixed_element, mixed_element)
